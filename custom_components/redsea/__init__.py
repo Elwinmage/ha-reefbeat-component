94be150665fd96fccd7108818b4b4fab2122a73e
root <root@homeassistant.local.hass.io>
2025-07-02 13:40:14 +0200

    reuse async-setup

--- custom_components/redsea/__init__.py
+++ custom_components/redsea/__init__.py
@@ -28,6 +28,12 @@ import traceback
 
 _LOGGER = logging.getLogger(__name__)
 
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the ReefBeat component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Creation des entités à partir d'une configEntry"""
 
